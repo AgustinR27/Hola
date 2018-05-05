a7c54cff456ee74228bf0875c425c1dfee5fa1bb
mauro-cuquejo <mauro.cuquejo@gmail.com>
2018-05-05 20:35:13 -0300

    genero los diccionarios de palabras y jugadores

--- TP_auxiliar.py
+++ TP_auxiliar.py
@@ -16,7 +16,7 @@ def formatearPalabra(palabra):
 def esPalabraValida(palabra):
     return palabra.isalpha() and len(palabra) >= 5
 
-def generarDiccionario():
+def generarDiccionarioPalabras():
     #a partir del string pasado por los profesores, se genera un diccionario de palabras con el siguiente formato:
     #clave = palabra valor = lista compuesta por [cantidad_de_repeticiones, palabra_ya_utilizada]
     texto = obtener_texto()
@@ -32,3 +32,43 @@ def generarDiccionario():
                         dic_palabras[formatearPalabra(palabra)][0] += 1
 
     return dic_palabras
+
+def solicitarValor(mensaje):
+    valor = input(mensaje)
+    return valor
+
+def esCantidadValida(cantidad_jugadores):
+    return cantidad_jugadores > 1 and cantidad_jugadores <= 10
+
+
+def solicitarCantJugadores():
+    cant_jugadores = int(solicitarValor("Ingrese cantidad de jugadores"))
+    while not esCantidadValida(cant_jugadores):
+        cant_jugadores = int(solicitarValor("Cantidad incorrecta, tienen que ser al menos dos y máximo diez jugadores.\n Ingrese cantidad de jugadores:"))
+    return cant_jugadores
+
+def esNombreValido(nombre_jugador):
+    #pendiente. Hay que validar que sea un nombre correcto. ¿verificamos acá que no sea una clave ya usada en el diccionario?
+    return True
+
+def solicitarNombreJugador():
+    jugador = str(solicitarValor("Ingrese Nombre Jugador"))
+    while not esNombreValido(jugador):
+        jugador = int(solicitarValor("Nombre incorrecto.\n Ingrese Nombre Jugador:"))
+    return jugador
+
+def generarDiccionarioJugadores(cant_jugadores):
+    #a partir de una cantidad de jugadores pasada por parametro, se solicita dicha cantidad de veces el nombre de jugadores. Se valida que los nombres no hayan sido utilizados ya en el diccionario.
+    #el cual tiene el siguiente formato:
+    #clave = jugador valor = lista compuesta por [puntaje, lista de palabras utilizadas, ganador_ultima_partida?, jugador_eliminado?]
+    dic_jugadores = {}
+    for numero_jugador in range(cant_jugadores):
+        jugador = solicitarNombreJugador()
+        if formatearPalabra(jugador) not in dic_jugadores:
+            dic_jugadores[formatearPalabra(jugador)] = [0, [], False, False]
+        else:
+            while formatearPalabra(jugador) in dic_jugadores:
+                print("El nombre ingresado ya fue utilizado por otra persona. Ingrese un nombre distinto")
+                jugador = solicitarNombreJugador()
+            dic_jugadores[formatearPalabra(jugador)] = [0, [], False, False]
+    return dic_jugadores
--- TP_main.py
+++ TP_main.py
@@ -1,6 +1,12 @@
-from TP_auxiliar import generarDiccionario
+from TP_auxiliar import generarDiccionarioPalabras
+from TP_auxiliar import solicitarCantJugadores
+from TP_auxiliar import  generarDiccionarioJugadores
+
 def jugar_ahorcado():
-    diccionario_palabras = generarDiccionario()
+    diccionario_palabras = generarDiccionarioPalabras()
     print(diccionario_palabras.keys())
+    cant_jugadores = solicitarCantJugadores()
+    diccionario_jugadores = generarDiccionarioJugadores(cant_jugadores)
+    print(diccionario_jugadores.keys())
 
 jugar_ahorcado()
