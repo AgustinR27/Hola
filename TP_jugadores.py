3be817ad0199616231f20e9bdc36d11b2d03a33f
Daro Gimenez <38989253+dasegc@users.noreply.github.com>
2018-05-05 12:14:27 -0300

    Añadí las funciones solicitarNombreJugador y cant_personas Fijense que funcionan, aclaro que el texto que imprime es solo demostrativo.

--- TP_jugadores.py
+++ TP_jugadores.py
@@ -1,11 +1,20 @@
 from TP_general import solicitarValor
 
-def ingresarNombrePersonas(cant_personas):
-    #pide nombre para la cantidad de personas
+def solicitarNombreJugador():
+    # pide nombre para la cantidad de personas
     lista_personas = []
-    for indice in range(cant_personas):
-        lista_personas.append(solicitarValor("Ingrese nombre del jugador {0}".format(indice+1)))
+    for indice in range(cant_personas()):
+        lista_personas.append(solicitarValor())
     return lista_personas
+
+def cant_personas():
+    personas = int(input('Ingrese el número de personas a ahorcar: '))
+    if personas <= 10:
+        jugadores = personas
+    else:
+        print('Nos hemos quedado sin sogas para tantos cuellos!!! Solo contamos con 10. Intenta nuevamente...')
+        jugadores = cant_personas()
+    return jugadores
 
 def mostrarDatosJugador(jugador):
     print("datos jugador")
