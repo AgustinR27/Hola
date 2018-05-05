2fff0203e6555ab314bed7691fa27a34c5aa79f9
Daro Gimenez <38989253+dasegc@users.noreply.github.com>
2018-05-05 20:56:23 -0300

    Defini turni_Aleatorio....Ahora pisemosno Mauro!!!

--- TP_auxiliar.py
+++ TP_auxiliar.py
@@ -72,3 +72,9 @@ def generarDiccionarioJugadores(cant_jugadores):
                 jugador = solicitarNombreJugador()
             dic_jugadores[formatearPalabra(jugador)] = [0,0, [], False, False]
     return dic_jugadores
+
+def turno_aleatorio(lista):
+    lista_ordenada = []
+    for i in range(len(lista)):
+        lista_ordenada.append(lista.pop(random.randint(0, len(lista) - 1)))
+    return lista_ordenada
