8eb300f7dfb00450a871a059cb6d89fc1dd018bd
LuanCorrionero <luancorrionero123@gmail.com>
2018-05-14 00:53:22 -0300

    agrego una funcion para actualizar puntaje

--- Luan.py
+++ Luan.py
@@ -2,6 +2,18 @@
 
 from TP_auxiliar import *
 
+"""Se pide el diccionario y el jugador a quien se le sumará puntos. El jugador (clave del diccionario) sumara 1 punto cada
+vez que se llame la funcion"""
+
+
+def actualizarpuntos(diccionario_jugador, jugador, acierto, victoria):
+    if acierto == True:
+        diccionario_jugador[jugador][1] += 1
+    else:
+        diccionario_jugador[jugador][1] -= 2
+    if victoria == True:
+        diccionario_jugador[jugador][1] += 30
+    return diccionario_jugador
 
 
 
